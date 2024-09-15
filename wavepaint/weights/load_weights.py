55e5c8503688fa7e2641840aa5bc192a386c32a5
Cloudwalker <pranav13phoenix@gmail.com>
2024-09-15 13:04:32 +0530

    Update load_weights.py

--- wavepaint/weights/load_weights.py
+++ wavepaint/weights/load_weights.py
@@ -1,4 +1,4 @@
-
+import torch
 
 model = WavePaint(
 	num_modules	  = 8,
@@ -8,3 +8,10 @@ model = WavePaint(
 	final_dim 	  = 128,
 	dropout 	  = 0.5
 ).to(device)
+
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Weights/WavePaint__blocks4_dim128_modules8_places256_medium_mask.pth' #Places dataset trained with medium masks
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Weights/WavePaint__blocks4_dim128_modules8_places256_thick_mask.pth'  #Places dataset trained with thick masks
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Weights/WavePaint_blocks4_dim128_modules8_celebhq_medium_mask.pth'    #CelebAHQ dataset trained with medium masks
+url = 'https://huggingface.co/cloudwalker/wavemix/resolve/main/Weights/WavePaint_blocks4_dim128_modules8_celebhq_thick_mask.pth'     #CelebAHQ dataset trained with thick masks
+
+model.load_state_dict(torch.hub.load_state_dict_from_url(url))
